6520eec9e6bebea7d36cd8a308e445385408a76a
y-yana <66319933+y-yana@users.noreply.github.com>
2020-10-10 15:24:38 +0900

    aからLinkに変更

--- src/screens/Top.js
+++ src/screens/Top.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Top = () => {
   return (
     <div>
 		<h2>topだよ！</h2>
-		<a href="/quiz">quizをやる</a>
+		<Link to="/quiz">quizをやる</Link>
     </div>
   );
 };
