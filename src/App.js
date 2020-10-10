81339f52ec9d5ccc26ee6b647433c0d1e32b1c87
y-yana <66319933+y-yana@users.noreply.github.com>
2020-10-10 15:22:07 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Quiz from './screens/Quiz';
+import Top from './screens/Top';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
