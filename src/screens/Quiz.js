0f05895ac274e7b6ef25432a42ed6cf7b47c3321
y-yana <66319933+y-yana@users.noreply.github.com>
2020-10-10 16:21:40 +0900

    quizの取得後に保存

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -16,26 +16,76 @@ const config = {
 
 const Quiz = () => {
 
+  const [quizzes, setquizzes] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const [isCorrect, setCorrect] = useState(null);
+  const [answered, setAnswered] = useState(false);
+
     useEffect(() => {
     fetchQuiz();
     return;
   }, []);
 
   const fetchQuiz = () => {
-    // 情報を取得
+    setAnswered(false);
+    setLoading(true);
+    setSelectedAnswers([]);
     axios
       .get(url, config)
       .then((res) => {
-        console.log(res);
+        // console.log(res)
+        if (res.data) {
+          setquizzes(res.data[0]);
+        }
       })
       .catch((e) => {
         alert("エラーだよ。コンソールを見てね");
         console.error(e);
       })
       .finally(() => {
-        console.log("done!");
+        setLoading(false);
       });
   };
+
+  const checkAnswer = () => {
+    // 処理しやすい形に変換
+    const realAnswer = quizList
+      .map((item, index) => ({
+        index: index,
+        isCorrect: item.isCorrect,
+      }))
+      .filter((item) => item.isCorrect)
+      .map((item) => item.index)
+      .sort();
+    setCorrect(
+      JSON.stringify(realAnswer) === JSON.stringify(selectedAnswers.sort())
+    );
+    setAnswered(true);
+  };
+
+  const updateAnswer = (id, append) => {
+    if (append) {
+      setSelectedAnswers([...selectedAnswers, id]);
+    } else {
+      setSelectedAnswers([...selectedAnswers].filter((item) => item !== id));
+    }
+  };
+
+  // JSONは真偽値も表せるはずなのにstringで渡してきやがるからparseする
+  const correctAnswers =
+    quizzes &&
+    Object.values(quizzes.correct_answers).map((item) => JSON.parse(item));
+
+  // 処理しやすい形にしておく
+  const quizList =
+    quizzes &&
+    Object.values(quizzes.answers)
+      .filter((choice) => !!choice)
+      .map((question, index) => ({
+        choice: question,
+        isCorrect: correctAnswers[index],
+      }));
 
   return (
     <div>
@@ -43,6 +93,7 @@ const Quiz = () => {
 		<Link to="/">topに戻る</Link>
     </div>
   );
+
 };
 
 export default Quiz;
